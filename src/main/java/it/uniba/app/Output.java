cafacf1b616f03a051dabfdc34247821f8a54ff8
Giuseppe Ventrella <giuseppe.ventrella01@gmail.com>
2023-05-16 00:04:05 +0200

    sistemata formattazione spazi vuoti in Output.java

--- src/main/java/it/uniba/app/Output.java
+++ src/main/java/it/uniba/app/Output.java
@@ -7,7 +7,7 @@ public final class Output {
 
     private static final String BENVENUTO =
     "                             BENVENUTO NEL GIOCO DELLA BATTAGLIA NAVALE                             \n";
-    
+
     private static final String DESCRIZIONE =
       " +-------------------------------------------------------------------------------------------------+\n"
     + "|                                                                                                  |\n"
@@ -15,10 +15,10 @@ public final class Output {
     + "|                    all'interno della quale saranno posizionate 10 navi.                          |\n"
     + "|                  L'obiettivo del gioco è quello di trovarle e affondarle,                        |\n"
     + "|                indicando ad ogni turno le coordinate della cella che vuoi colpire.               |\n"
-    + "|          Se riuscirai ad affondarle tutte, entro il numero massimo di tentativi falliti          |\n" 
-    + "|              (sarà impostato di default il livello facile), avrai vinto la partita.              |\n" 
-    + "|                  Mi raccomando! Le coordinate devono essere espresse nella forma:                |\n" 
-    + "|                                         [colonna][riga]                                          |\n" 
+    + "|          Se riuscirai ad affondarle tutte, entro il numero massimo di tentativi falliti          |\n"
+    + "|              (sarà impostato di default il livello facile), avrai vinto la partita.              |\n"
+    + "|                  Mi raccomando! Le coordinate devono essere espresse nella forma:                |\n"
+    + "|                                         [colonna][riga]                                          |\n"
     + "| (dove le colonne sono inidicate dalle lettere dalla A alla J, e le righe dai numeri da 1 a 10).  |\n"
     + "+--------------------------------------------------------------------------------------------------+";
 
@@ -26,17 +26,17 @@ public final class Output {
       "|                                                                                                  |\n"
     + "|                                               COMANDI                                            |\n"
     + "|                Di seguito ti lascio una lista dei COMANDI che potrai utilizzare:                 |\n"
-    + "|                      - /help : per visualizzare di nuovo questo messaggio.                       |\n" 
-    + "|                      - /gioca : per avviare una nuova partita                                    |\n" 
+    + "|                      - /help : per visualizzare di nuovo questo messaggio.                       |\n"
+    + "|                      - /gioca : per avviare una nuova partita                                    |\n"
     + "|                      - /esci : per uscire dal gioco                                              |\n"
     + "|                      - /facile : per impostare il livello della partita a facile                 |\n"
-    + "|                                  (MAX 50 tentativi falliti)                                      |\n" 
-    + "|                      - /medio : per impostare il livello della partita a media                   |\n" 
-    + "|                                 (MAX 30 tentativi falliti)                                       |\n" 
-    + "|                      - /difficile : per impostare il livello della partita a difficile           |\n" 
-    + "|                                    (MAX 10 tentativi falliti)                                    |\n" 
+    + "|                                  (MAX 50 tentativi falliti)                                      |\n"
+    + "|                      - /medio : per impostare il livello della partita a media                   |\n"
+    + "|                                 (MAX 30 tentativi falliti)                                       |\n"
+    + "|                      - /difficile : per impostare il livello della partita a difficile           |\n"
+    + "|                                    (MAX 10 tentativi falliti)                                    |\n"
     + "|                      - /mostralivello : per mostrare il livello di difficoltà impostato          |\n"
-    + "|                      - /mostranavi : per mostrare il tipo, la dimensione e il numero             |\n" 
+    + "|                      - /mostranavi : per mostrare il tipo, la dimensione e il numero             |\n"
     + "|                                      delle navi che devi ancora affondare                        |\n"
     + "|                      - /svelagriglia : per mostrare la griglia e tutte le navi posizionate       |\n"
     + "+--------------------------------------------------------------------------------------------------+\n\n";
@@ -46,7 +46,7 @@ public final class Output {
      */
     private Output() {
     }
-    
+
     /**
      * stampa la stringa di Benvenuto.
      */
