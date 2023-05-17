ab25d16ed1c3d4f0a94b4c9d9aaa4f1945c0b838
giosteh <stea.giovanni.56@gmail.com>
2023-05-17 18:33:29 +0200

    Aggiunta classe Griglia.java

---
+++ src/main/java/it/uniba/app/Griglia.java
@@ -0,0 +1,11 @@
+package it.uniba.app;
+
+/**
+ * Classe che rappresenta il campo da gioco sotto forma di griglia.
+ */
+public final class Griglia {
+
+
+    private static final int DIMENSIONE = 10;
+
+}
