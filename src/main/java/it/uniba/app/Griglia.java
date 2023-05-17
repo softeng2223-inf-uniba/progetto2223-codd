4f0defd557f62ca759c8dad900aee30ab7b0ef63
giosteh <stea.giovanni.56@gmail.com>
2023-05-17 18:39:42 +0200

    Aggiunta inner class Cella con costruttori in Griglia.java

--- src/main/java/it/uniba/app/Griglia.java
+++ src/main/java/it/uniba/app/Griglia.java
@@ -5,7 +5,34 @@ package it.uniba.app;
  */
 public final class Griglia {
 
+    /**
+     * Classe interna che rappresenta la singola cella.
+     */
+    public final class Cella {
+
+        private final int riga;
+        private final int colonna;
+        private boolean colpita = false;
+
+        /**
+         * Costruttore vuoto.
+         */
+        private Cella() {
+        }
+
+        /**
+         * Costruttore che inizializza riga e colonna della cella.
+         * @param rigaX
+         * @param colonnaY
+         */
+        public Cella(final int rigaX, final int colonnaY) {
+            this.riga = rigaX;
+            this.colonna = colonnaY;
+        }
+
+    }
 
     private static final int DIMENSIONE = 10;
+
 
 }
