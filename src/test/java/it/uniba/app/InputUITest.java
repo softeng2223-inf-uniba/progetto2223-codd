383284898005daf694fec1f087fce6845192680a
Lorenzo Scazzari <lorenzoscazzari3@gmail.com>
2023-06-08 19:17:08 +0200

    Modicati casi di test in InputUITest

--- src/test/java/it/uniba/app/InputUITest.java
+++ src/test/java/it/uniba/app/InputUITest.java
@@ -10,6 +10,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class InputTest {
 
     @Test
+    public void testAcquisisciConfermaSi() {
+        InputUI inputUI = new InputUI();
+        String input = "SI";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertTrue(inputUI.acquisisciConferma(new Scanner(System.in)));
+    }
+
+    @Test
+    public void testAcquisisciConfermaNo() {
+        InputUI inputUI = new InputUI();
+        String input = "NO";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertFalse(inputUI.acquisisciConferma(new Scanner(System.in)));
+    }
+
+    @Test
     public void testIsTentativoValido() {
         InputUI inputUI = new InputUI();
         String[] comando = {"A", "1"};
@@ -21,20 +39,6 @@ class InputTest {
         InputUI inputUI = new InputUI();
         String[] comando = {"/mostranavi"};
         assertFalse(inputUI.isTentativo(comando));
-    }
-
-    @Test
-    public void testParseInputComandoValido() {
-        InputUI inputUI = new InputUI();
-        boolean result = inputUI.parseInput("/gioca", InputUI.StatoGioco.SESSIONE);
-        assertTrue(result);
-    }
-
-    @Test
-    public void testParseInputComandoConTreToken() {
-        InputUI inputUI = new InputUI();
-        boolean result = inputUI.parseInput("/comando param1 param2", InputUI.StatoGioco.SESSIONE);
-        assertFalse(result);
     }
 
     @Test
@@ -81,171 +85,4 @@ class InputTest {
         assertEquals("10", result[1]);
     }
 
-    @Test
-    void testVerificaComandoValidoInSessione() {
-
-	    InputUI inputUI = new InputUI();
-        String[] comando = {"/gioca", null};
-
-        InputUI.StatoGioco contesto = InputUI.StatoGioco.SESSIONE;
-
-        boolean result = InputUI.verificaComando(comando, contesto);
-
-        assertTrue(result);
-    }
-
-    @Test
-    void testVerificaComandoValidoInPartita() {
-
-	    InputUI inputUI = new InputUI();
-        String[] comando = {"/mostranavi", null};
-        InputUI.StatoGioco contesto = InputUI.StatoGioco.PARTITA;
-
-        boolean result = InputUI.verificaComando(comando, contesto);
-
-        assertTrue(result);
-    }
-
-    @Test
-    void testVerificaComandoInvalidoInSessione() {
-
-	    InputUI inputUI = new InputUI();
-        String[] comando = {"/nonvalido", null};
-        InputUI.StatoGioco contesto = InputUI.StatoGioco.SESSIONE;
-
-        boolean result = InputUI.verificaComando(comando, contesto);
-
-        assertFalse(result);
-    }
-
-    @Test
-    void testVerificaComandoInvalidoInPartita() {
-
-	    InputUI inputUI = new InputUI();
-        String[] comando = {"/nonvalido", null};
-        InputUI.StatoGioco contesto = InputUI.StatoGioco.PARTITA;
-
-        boolean result = InputUI.verificaComando(comando, contesto);
-
-        assertFalse(result);
-    }
-
-    @Test
-    void testVerificaComandoNonPermessoInSessione() {
-	
-	    InputUI inputUI = new InputUI();
-        String[] comando = {"/svelagriglia", null};
-        InputUI.StatoGioco contesto = InputUI.StatoGioco.SESSIONE;
-
-        boolean result = InputUI.verificaComando(comando, contesto);
-
-        assertFalse(result);
-    }
-
-    @Test
-    void testVerificaComandoNonPermessoInPartita() {
-
-	    InputUI inputUI = new InputUI();
-        String[] comando = {"/gioca", null};
-        InputUI.StatoGioco contesto = InputUI.StatoGioco.PARTITA;
-
-        boolean result = InputUI.verificaComando(comando, contesto);
-
-        assertFalse(result);
-    }
-
-    @Test
-    void testVerificaComandoSpecialeSenzaNumero() {
-
-	    InputUI inputUI = new InputUI();
-        String[] comando = {"/tentativi", null};
-        InputUI.StatoGioco contesto = InputUI.StatoGioco.SESSIONE;
-
-        boolean result = InputUI.verificaComando(comando, contesto);
-
-        assertFalse(result);
-    }
-
-    @Test
-    void testVerificaComandoSpecialeConNumeroNegativo() {
-
-	    InputUI inputUI = new InputUI();
-        String[] comando = {"/tentativi", "-5"};
-        InputUI.StatoGioco contesto = InputUI.StatoGioco.SESSIONE;
-
-        boolean result = InputUI.verificaComando(comando, contesto);
-
-        assertFalse(result);
-    }
-
-    @Test
-    void testVerificaComandoSpecialeConNumero() {
-
-	    InputUI inputUI = new InputUI();
-        String[] comando = {"/tentativi", "15"};
-        InputUI.StatoGioco contesto = InputUI.StatoGioco.SESSIONE;
-
-        boolean result = InputUI.verificaComando(comando, contesto);
-
-        assertTrue(result);
-    }
-
-    @Test
-    void testVerificaComandoValidoAncheConNumero() {
-
-	    InputUI inputUI = new InputUI();
-        String[] comando = {"/facile", "10"};
-        InputUI.StatoGioco contesto = InputUI.StatoGioco.SESSIONE;
-
-        boolean result = InputUI.verificaComando(comando, contesto);
-
-        assertTrue(result);
-    }
-
-    @Test
-    void testVerificaComandoValidoAncheSenzaNumero() {
-
-	    InputUI inputUI = new InputUI();
-        String[] comando = {"/facile", null};
-        InputUI.StatoGioco contesto = InputUI.StatoGioco.SESSIONE;
-
-        boolean result = InputUI.verificaComando(comando, contesto);
-
-        assertTrue(result);
-    }
-
-    @Test
-    void testVerificaComandoNonValidoConNumero() {
-
-	    InputUI inputUI = new InputUI();
-        String[] comando = {"/gioca", "10"};
-        InputUI.StatoGioco contesto = InputUI.StatoGioco.SESSIONE;
-
-        boolean result = InputUI.verificaComando(comando, contesto);
-
-        assertTrue(result);
-    }
-
-
-    @Test
-    void testVerificaNumeroValido() {
-
-	    InputUI inputUI = new InputUI();
-        String numero = "10";
-
-        boolean result = InputUI.verificaNumero(numero);
-
-        assertTrue(result);
-    }
-
-    @Test
-    void testVerificaNumeroInvalido() {
-
-	    InputUI inputUI = new InputUI();
-        String numero = "-5";
-
-        boolean result = InputUI.verificaNumero(numero);
-
-        assertFalse(result);
-    }
 }
