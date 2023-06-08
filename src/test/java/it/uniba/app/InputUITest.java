f68d369bb11046baa1df5a8644732651e603290d
Lorenzo Scazzari <lorenzoscazzari3@gmail.com>
2023-06-08 20:22:38 +0200

    Aggiunti commenti relativi ai casi di test in InputUITest

--- src/test/java/it/uniba/app/InputUITest.java
+++ src/test/java/it/uniba/app/InputUITest.java
@@ -1,84 +1,98 @@
 package it.uniba.app;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class InputTest {
 
     @Test
-    public void testAcquisisciConfermaSi() {
+    @DisplayName("Restituisce vero se l'input è 'si'")
+    void testAcquisisciConfermaSi() {
         InputUI inputUI = new InputUI();
         String input = "SI";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
-        assertTrue(inputUI.acquisisciConferma(new Scanner(System.in)));
+        assertTrue(inputUI.acquisisciConferma(new Scanner(System.in,"UTF-8")));
     }
 
     @Test
-    public void testAcquisisciConfermaNo() {
+    @DisplayName("Restituisce falso se l'input è 'no'")
+    void testAcquisisciConfermaNo() {
         InputUI inputUI = new InputUI();
         String input = "NO";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
-        assertFalse(inputUI.acquisisciConferma(new Scanner(System.in)));
+        assertFalse(inputUI.acquisisciConferma(new Scanner(System.in,"UTF-8")));
     }
 
     @Test
-    public void testIsTentativoValido() {
+    @DisplayName("Restituisce true se il tentativo è valido")
+    void testIsTentativoValido() {
         InputUI inputUI = new InputUI();
         String[] comando = {"A", "1"};
         assertTrue(inputUI.isTentativo(comando));
     }
 
     @Test
-    public void testIsTentativoNonValido() {
+    @DisplayName("Restituisce false se il tentativo non è valido")
+    void testIsTentativoNonValido() {
         InputUI inputUI = new InputUI();
         String[] comando = {"/mostranavi"};
         assertFalse(inputUI.isTentativo(comando));
     }
 
     @Test
-    public void testAcquisisciComandoAzioneValidaInSessione() {
+    @DisplayName("Assicura che il comando sia valido in sessione")
+    void testAcquisisciComandoAzioneValidaInSessione() {
         InputUI inputUI = new InputUI();
         String input = "/help";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
         String[] expected = {"/help", null};
-        assertArrayEquals(expected, inputUI.acquisisciComando(new Scanner(System.in), InputUI.StatoGioco.SESSIONE));
+        assertArrayEquals(expected, inputUI.acquisisciComando(new Scanner(System.in,"UTF-8"), InputUI.StatoGioco.SESSIONE));
     }
 
     @Test
-    public void testAcquisisciComandoAzioneValidaInPartita() {
+    @DisplayName("Assicura che il comando sia valido in partita")
+    void testAcquisisciComandoAzioneValidaInPartita() {
         InputUI inputUI = new InputUI();
         String input = "/mostranavi";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
         String[] expected = {"/mostranavi", null};
-        assertArrayEquals(expected, inputUI.acquisisciComando(new Scanner(System.in), InputUI.StatoGioco.PARTITA));
+        assertArrayEquals(expected, inputUI.acquisisciComando(new Scanner(System.in,"UTF-8"), InputUI.StatoGioco.PARTITA));
     }
 
     @Test
-    public void testAcquisisciComandoTentativoValido() {
+    @DisplayName("Assicura che il tentativo sia valido in partita")
+    void testAcquisisciComandoTentativoValido() {
         InputUI inputUI = new InputUI();
         String input = "A-1";
+        int limite = 10;
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
         String[] expected = {"A", "1"};
-        assertArrayEquals(expected, inputUI.acquisisciComando(new Scanner(System.in), InputUI.StatoGioco.PARTITA, 10));
+        assertArrayEquals(expected, inputUI.acquisisciComando(new Scanner(System.in,"UTF-8"), InputUI.StatoGioco.PARTITA, limite));
     }
 
 
     @Test
-    public void testAcquisisciComandoConNumeroSessioneValido() {
+    @DisplayName("Assicura che il comando con numero sia valido in sessione")
+    void testAcquisisciComandoConNumeroSessioneValido() {
         String input = "/tentativi 10";
+        int limite = 10;
         ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
-        Scanner scanner = new Scanner(inputStream);
+        Scanner scanner = new Scanner(inputStream, "UTF-8");
         InputUI inputUI = new InputUI();
-        String[] result = inputUI.acquisisciComando(scanner, InputUI.StatoGioco.SESSIONE, 10);
+        String[] result = inputUI.acquisisciComando(scanner, InputUI.StatoGioco.SESSIONE, limite);
         assertEquals("/tentativi", result[0]);
         assertEquals("10", result[1]);
     }
