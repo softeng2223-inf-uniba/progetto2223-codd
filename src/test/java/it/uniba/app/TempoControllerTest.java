a415ebbf7a7440b20215725e9f13e656996f29ce
Lorenzo Scazzari <lorenzoscazzari3@gmail.com>
2023-06-09 18:03:31 +0200

    Risolti problemi CheckStyle in TempoControllerTest.java

--- src/test/java/it/uniba/app/TempoControllerTest.java
+++ src/test/java/it/uniba/app/TempoControllerTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
