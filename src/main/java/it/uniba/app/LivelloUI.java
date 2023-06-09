f19c510ad1eda65d4812bb75659d645e40a924d3
RaffaeleNacci <raffaele.nacci99@gmail.com>
2023-06-09 12:16:38 +0200

    Aggiornata classe LivelloUI.java

--- src/main/java/it/uniba/app/LivelloUI.java
+++ src/main/java/it/uniba/app/LivelloUI.java
@@ -39,9 +39,14 @@ public final class LivelloUI {
             break;
         }
 
-        System.out.println("Ok.");
+        System.out.println("\n: Ok.");
     }
 
+    /**
+     * Metodo che verifica se un comando è un'impostazione di livello.
+     * @param comando
+     * @return true se il comando è un'impostazione di livello, false altrimenti.
+     */
     public boolean isImpostazioneLivello(final String[] comando) {
 
         if (comando[1] == null) {
@@ -50,7 +55,57 @@ public final class LivelloUI {
         return false;
     }
 
-    public void impostaTentativiLivello(final String[] comando) {
+    /**
+     * Metodo che imposta i tentativi associati ad un certo livello.
+     * @param comando
+     */
+    public void impostaTentativiPerLivello(final String[] comando) {
 
+        ProprietaPartita prop = ProprietaPartita.getIstanza();
+        int tent = Integer.parseInt(comando[1]);
+
+        if (tent > prop.getDimensioniGriglia()) {
+            System.out.println("\n: Non puoi impostare un numero di tentativi maggiore della grandezza della griglia!");
+        } else {
+            switch (comando[0]) {
+                case "/facile":
+                    this.livController.impostaTentativiLivello(Livello.Tipo.FACILE, tent);
+                break;
+                case "/medio":
+                    this.livController.impostaTentativiLivello(Livello.Tipo.MEDIO, tent);
+                break;
+                case "/difficile":
+                    this.livController.impostaTentativiLivello(Livello.Tipo.DIFFICILE, tent);
+                break;
+                default:
+                break;
+            }
+        }
+    }
+
+    /**
+     * Metodo che permette di impostare un numero di tentativi personalizzato.
+     * @param comando
+     */
+    public void impostaTentativiPerLivelloPersonalizzato(final String[] comando) {
+
+        ProprietaPartita prop = ProprietaPartita.getIstanza();
+        int tent = Integer.parseInt(comando[1]);
+
+        if (tent > prop.getDimensioniGriglia()) {
+            System.out.println("\n: Non puoi impostare un numero di tentativi maggiore della grandezza della griglia!");
+        } else {
+            this.livController.impostaTetativiPersonalizzato(tent);
+        }
+    }
+
+    /**
+     * Metodo che stampa sulla console del terminale il livello impostato
+     * e il numero di tentativi associato.
+     */
+    public void displayLivelloCorrente() {
+
+        System.out.println("\n: Il livello impostato è: " + this.livController.ottieniLivelloCorrente()
+        + "\n: Hai a disposizione: " + this.livController.ottieniTentativiCorrenti() + " tentativi.");
     }
 }
