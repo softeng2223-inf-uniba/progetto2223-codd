6ca31ef936043f24204d34a489fdf6e1d4547daf
matera02 <m.matera51@studenti.uniba.it>
2023-06-13 19:46:38 +0200

    Aggiornata App.java con engine partita

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -1,6 +1,9 @@
 package it.uniba.app;
 
+import java.util.PropertyResourceBundle;
 import java.util.Scanner;
+
+import main.java.it.uniba.app.PartitaUI;
 
 /**
  * Classe Main dell'applicazione.
@@ -21,6 +24,12 @@ public final class App {
      * @param args Argomenti linea di comando.
      */
     public static void main(final String[] args) {
+        HelpUI helpUI = new HelpUI();
+        if (args[0].equals("--help") || args[0].equals("-h")) {
+            helpUI.displayHelp();
+            return;
+        }
+        helpUI.displayHelp();
 
         Proprieta prop = Proprieta.getIstanza();
         Livello liv = Livello.getIstanza();
@@ -42,12 +51,14 @@ public final class App {
         InputUI input = new InputUI();
         InputUI.StatoGioco contesto = InputUI.StatoGioco.SESSIONE;
 
-        LivelloUI livUI;
-        LivelloController livContr;
-        TempoUI tempUI;
-        TempoController tempContr;
-        ProprietaUI propUI;
-        ProprietaController propContr;
+        LivelloController livContr = new LivelloController(liv);
+        TempoController tempContr = new TempoController(temp);
+        ProprietaController propContr = new ProprietaController(prop);
+
+        LivelloUI livUI = new LivelloUI(livContr);
+        TempoUI tempUI = new TempoUI(tempContr);
+        ProprietaUI propUI = new ProprietaUI(propContr);
+        HelpUI helpUI = new HelpUI();
 
         while (true) {
             comando = input.acquisisciComando(tastiera, contesto);
@@ -55,8 +66,6 @@ public final class App {
                 case "/facile":
                 case "/medio":
                 case "/difficile":
-                    livContr = new LivelloController(liv);
-                    livUI = new LivelloUI(livContr);
                     if (livUI.isImpostazioneLivello(comando)) {
                         livUI.impostaLivello(comando);
                     } else {
@@ -64,25 +73,17 @@ public final class App {
                     }
                 break;
                 case "/tentativi":
-                    livContr = new LivelloController(liv);
-                    livUI = new LivelloUI(livContr);
                     livUI.impostaTentativiPerLivelloPersonalizzato(comando);
                 break;
                 case "/mostralivello":
-                    livContr = new LivelloController(liv);
-                    livUI = new LivelloUI(livContr);
                     livUI.displayLivelloCorrente();
                 break;
                 case "/tempo":
-                    tempContr = new TempoController(temp);
-                    tempUI = new TempoUI(tempContr);
                     tempUI.impostaTempoDiGioco(comando);
                 break;
                 case "/standard":
                 case "/large":
                 case "/extralarge":
-                    propContr = new ProprietaController(prop);
-                    propUI = new ProprietaUI(propContr);
                     propUI.impostaDimensioniGriglia(comando);
                 break;
                 case "/esci":
@@ -90,10 +91,87 @@ public final class App {
                         return;
                     }
                 break;
+                case "/help":
+                    helpUI.displayHelp();
+                break;
+                case "/gioca":
+                    enginePartita(tastiera, liv, temp);
+                break;
                 default:
                 break;
             }
         }
+        tastiera.close();
+    }
+
+    /**
+     * Metodo che rappresenta il motore della partita.
+     * @param tastiera
+     * @param liv
+     * @param temp
+     */
+    private void enginePartita(final Scanner tastiera, final Livello liv, final Tempo temp) {
+
+        String[] comando;
+        InputUI input = new InputUI();
+        InputUI.StatoGioco contesto = InputUI.StatoGioco.PARTITA;
+
+        Partita partita = new Partita();
+
+        TempoController tempContr = new TempoController(temp);
+        LivelloController livContr = new LivelloController(liv);
+        InizioPartitaController ipContr = new InizioPartitaController(partita);
+        PartitaInCorsoController pcContr = new PartitaInCorsoController(partita);
+        FinePartitaController fpContr = new FinePartitaController(partita);
+        
+        GrigliaUI grigliaUI = new GrigliaUI(pcContr);
+        PartitaUI partitaUI = new PartitaUI(ipContr, pcContr, fpContr);
+        TempoUI tempUI = new TempoUI(tempContr);
+        LivelloUI livUI = new LivelloUI(livContr);
+        HelpUI helpUI = new HelpUI();
+
+        partitaUI.inizia();
+        while (true) {
+            comando = input.acquisisciComando(tastiera, contesto);
+            if (input.isTentativo(comando)) {
+                partitaUI.effettuaTentativo(comando);
+            } else {
+                switch (comando[0]) {
+                    case "/mostratentativi":
+                        partitaUI.displayTentativi();
+                    break;
+                    case "/mostratempo":
+                        tempUI.displayTempo();
+                    break;
+                    case "/mostralivello":
+                        livUI.displayLivelloCorrente();
+                    break;
+                    case "/mostragriglia":
+                        grigliaUI.displayGriglia();
+                    break;
+                    case "/svelagriglia":
+                        grigliaUI.displayGrigliaSvelata();
+                    break;
+                    case "/mostranavi":
+                        partitaUI.displayNavi();
+                    break;
+                    case "/abbandona":
+                        if (partitaUI.abbandona(tastiera)) {
+                            return;
+                        }
+                    break;
+                    case "/help":
+                        helpUI.displayHelp();
+                    break;
+                    default:
+                    break;
+                }
+            }
+            if (partitaUI.isTerminata()) {
+                break;
+            }
+        }
+
     }
 }
 
